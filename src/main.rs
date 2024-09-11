564ec49a1e165846f7da221544f73de55650940d
Kei-K23 <keiksl2301@gmail.com>
2024-09-11 10:57:38 +0630

    Testing concurrent request and making async and blocking working together

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use serde_json::{from_str, to_string_pretty, Value};
 use std::fs::File;
 use std::io::{self, Read, Write};
 use std::{path::Path, str::FromStr, time::Duration};
+use tokio::task;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // CLI interface
     let matches = Command::new("curlio")
         .version("0.3.0")
@@ -105,6 +107,12 @@ fn main() {
                 .short('D')
                 .long("download")
         )
+        .arg(
+            Arg::new("concurrent")
+                .help("Send requests concurrently with multiple URLs (comma-separated URLs) <f for False/ t for True>")
+                .long("concurrent")
+                .default_value("f")
+        )
         .get_matches();
 
     // Get url value
@@ -127,6 +135,7 @@ fn main() {
     let proxy: Option<&String> = matches.get_one::<String>("proxy");
     let follow_redirects = matches.get_one::<String>("follow").unwrap() == "t";
     let file_path = matches.get_one::<String>("download");
+    let is_concurrent_req = matches.get_one::<String>("concurrent").unwrap() == "t";
 
     // Get timeout value
     let timeout = matches.get_one::<String>("timeout");
@@ -135,130 +144,187 @@ fn main() {
         .get_one::<String>("retry")
         .map_or(0, |r| r.parse::<u32>().unwrap_or(0));
 
-    // Init HTTP client with optional timeout
-    let mut client_builder = Client::builder();
+    // Here check for blocking request or concurrent async request
+    if is_concurrent_req {
+        // Handle concurrent request
+        // Init HTTP client with optional timeout
+        let mut client_builder = reqwest::Client::builder();
 
-    if let Some(timeout_sec) = timeout {
-        client_builder = client_builder.timeout(Duration::from_secs(
-            timeout_sec.parse::<u64>().unwrap_or(10),
-        ));
-    }
-
-    // Setup follow redirects
-    if follow_redirects {
-        client_builder = client_builder.redirect(reqwest::redirect::Policy::limited(10));
-    }
-
-    // Setup proxy
-    if let Some(proxy_schema) = proxy {
-        client_builder = client_builder.proxy(reqwest::Proxy::all(proxy_schema).unwrap());
-    }
-
-    let client = client_builder.build().unwrap();
-
-    // Create request builder
-    let mut req_builder = match method.to_uppercase().as_str() {
-        "GET" => client.get(url),
-        "POST" => client.post(url),
-        "PUT" => client.put(url),
-        "PATCH" => client.patch(url),
-        "DELETE" => client.delete(url),
-        _ => panic!("Unsupported method!"),
-    };
-
-    // Init headers map and add headers if provided
-    if let Some(headers_json) = headers {
-        let parsed_headers: Value = from_str(headers_json).unwrap();
-        let mut header_map: HeaderMap = HeaderMap::new();
-
-        if let Value::Object(header_obj) = parsed_headers {
-            for (key, value) in header_obj {
-                let header_key: HeaderName = HeaderName::from_str(&key).unwrap();
-                let header_value = HeaderValue::from_str(value.as_str().unwrap()).unwrap();
-                // Add header key value to header map
-                header_map.insert(header_key, header_value);
-            }
+        if let Some(timeout_sec) = timeout {
+            client_builder = client_builder.timeout(Duration::from_secs(
+                timeout_sec.parse::<u64>().unwrap_or(10),
+            ));
         }
-        // Add headers to request
-        req_builder = req_builder.headers(header_map);
-    }
 
-    // Attach data as the request body if provided
-    if let Some(data) = data {
-        req_builder = req_builder.body(data.clone());
-    }
+        // Setup follow redirects
+        if follow_redirects {
+            client_builder = client_builder.redirect(reqwest::redirect::Policy::limited(10));
+        }
 
-    // Set custom User-Agent
-    if let Some(user_agent_value) = user_agent {
-        req_builder = req_builder.header("User-Agent", user_agent_value);
-    }
+        // Setup proxy
+        if let Some(proxy_schema) = proxy {
+            client_builder = client_builder.proxy(reqwest::Proxy::all(proxy_schema).unwrap());
+        }
 
-    // Set basic authentication
-    if let Some(auth_value) = basic_auth {
-        // Define basic authentication
-        req_builder = req_builder.basic_auth(
-            auth_value.split(':').next().unwrap(),
-            Some(auth_value.split(':').nth(1).unwrap()),
-        );
-    }
+        let client = client_builder.build().unwrap();
 
-    // When -F flag is used, handle Multipart form (file upload)
-    if let Some(form_json) = form_data {
-        // Parse json string to value
-        let parsed_form: Value = from_str(form_json).unwrap();
-        // Init multipart form
-        let mut form_part = multipart::Form::new();
+        // Split the comma separated URLs
+        let url_lists: Vec<&str> = url.split(',').collect::<Vec<&str>>();
 
-        if let Value::Object(form_obj) = parsed_form {
-            // Loop through the Map
-            for (key, value) in form_obj {
-                if value.is_string() {
-                    // Get form value
-                    let form_value = value.as_str().unwrap();
+        // Use tokio tasks to concurrently send requests
+        let mut tasks = vec![];
 
-                    // Check if form value is a file
-                    if Path::new(form_value).exists() {
-                        // Add file path to form part
-                        form_part = form_part.file(key.clone(), form_value).unwrap();
-                    } else {
-                        // Normal form part
-                        form_part = form_part.text(key.clone(), form_value.to_string());
-                    }
+        for concurrent_url in url_lists {
+            let client_clone = client.clone();
+            let method_clone = method.clone();
+            let data_clone = data.clone().map(|d| d.to_string());
+
+            tasks.push(task::spawn(send_request_concurrently(
+                client_clone,
+                concurrent_url.to_string(),
+                method_clone,
+                data_clone,
+            )));
+        }
+
+        // Await all concurrent requests
+        let results = futures::future::join_all(tasks).await;
+
+        for result in results {
+            if let Ok(response) = result {
+                match response {
+                    Ok(resp) => handle_response_async(resp, verbose, silent, store).await,
+                    Err(e) => eprintln!("Error occurred: {}", e),
                 }
             }
         }
-
-        // Add multipart form instance to req builder
-        req_builder = req_builder.multipart(form_part);
-    }
-
-    // Send or retry (when it is in retry mode) the request
-    let response = if retry_count > 0 {
-        send_with_retry(req_builder, retry_count, verbose)
     } else {
-        match req_builder.send() {
-            Ok(res) => Some(res),
-            Err(err) => {
-                eprintln!("Request failed: {}", err);
-                None
-            }
-        }
-    };
+        // Handle blocking request
+        // Init HTTP client with optional timeout
+        let mut client_builder = Client::builder();
 
-    // Print response details
-    if let Some(res) = response {
-        // Here if download the file
-        if let Some(path) = file_path {
-            // Download process
-            if let Err(err) = download_file(res, &path) {
-                eprintln!("Download failed: {}", err);
+        if let Some(timeout_sec) = timeout {
+            client_builder = client_builder.timeout(Duration::from_secs(
+                timeout_sec.parse::<u64>().unwrap_or(10),
+            ));
+        }
+
+        // Setup follow redirects
+        if follow_redirects {
+            client_builder = client_builder.redirect(reqwest::redirect::Policy::limited(10));
+        }
+
+        // Setup proxy
+        if let Some(proxy_schema) = proxy {
+            client_builder = client_builder.proxy(reqwest::Proxy::all(proxy_schema).unwrap());
+        }
+
+        let client = client_builder.build().unwrap();
+        // Create request builder
+        let mut req_builder = match method.to_uppercase().as_str() {
+            "GET" => client.get(url),
+            "POST" => client.post(url),
+            "PUT" => client.put(url),
+            "PATCH" => client.patch(url),
+            "DELETE" => client.delete(url),
+            _ => panic!("Unsupported method!"),
+        };
+
+        // Init headers map and add headers if provided
+        if let Some(headers_json) = headers {
+            let parsed_headers: Value = from_str(headers_json).unwrap();
+            let mut header_map: HeaderMap = HeaderMap::new();
+
+            if let Value::Object(header_obj) = parsed_headers {
+                for (key, value) in header_obj {
+                    let header_key: HeaderName = HeaderName::from_str(&key).unwrap();
+                    let header_value = HeaderValue::from_str(value.as_str().unwrap()).unwrap();
+                    // Add header key value to header map
+                    header_map.insert(header_key, header_value);
+                }
+            }
+            // Add headers to request
+            req_builder = req_builder.headers(header_map);
+        }
+
+        // Attach data as the request body if provided
+        if let Some(data) = data {
+            req_builder = req_builder.body(data.clone());
+        }
+
+        // Set custom User-Agent
+        if let Some(user_agent_value) = user_agent {
+            req_builder = req_builder.header("User-Agent", user_agent_value);
+        }
+
+        // Set basic authentication
+        if let Some(auth_value) = basic_auth {
+            // Define basic authentication
+            req_builder = req_builder.basic_auth(
+                auth_value.split(':').next().unwrap(),
+                Some(auth_value.split(':').nth(1).unwrap()),
+            );
+        }
+
+        // When -F flag is used, handle Multipart form (file upload)
+        if let Some(form_json) = form_data {
+            // Parse json string to value
+            let parsed_form: Value = from_str(form_json).unwrap();
+            // Init multipart form
+            let mut form_part = multipart::Form::new();
+
+            if let Value::Object(form_obj) = parsed_form {
+                // Loop through the Map
+                for (key, value) in form_obj {
+                    if value.is_string() {
+                        // Get form value
+                        let form_value = value.as_str().unwrap();
+
+                        // Check if form value is a file
+                        if Path::new(form_value).exists() {
+                            // Add file path to form part
+                            form_part = form_part.file(key.clone(), form_value).unwrap();
+                        } else {
+                            // Normal form part
+                            form_part = form_part.text(key.clone(), form_value.to_string());
+                        }
+                    }
+                }
+            }
+
+            // Add multipart form instance to req builder
+            req_builder = req_builder.multipart(form_part);
+        }
+
+        // Send or retry (when it is in retry mode) the request
+        let response = if retry_count > 0 {
+            // task::spawn_blocking()
+            send_with_retry(req_builder, retry_count, verbose)
+        } else {
+            match req_builder.send() {
+                Ok(res) => Some(res),
+                Err(err) => {
+                    eprintln!("Request failed: {}", err);
+                    None
+                }
+            }
+        };
+
+        // Print response details
+        if let Some(res) = response {
+            // Here if download the file
+            if let Some(path) = file_path {
+                // Download process
+                if let Err(err) = download_file(res, &path) {
+                    eprintln!("Download failed: {}", err);
+                }
+            } else {
+                // Normal HTTP client request, response
+                handle_response(res, verbose, silent, store);
             }
         } else {
-            // Normal HTTP client request, response
-            handle_response(res, verbose, silent, store);
+            eprintln!("Failed to get response from server")
         }
-    } else {
-        eprintln!("Failed to get response from server")
     }
 }
 
@@ -293,6 +359,42 @@ fn handle_response(response: Response, verbose: bool, silent: bool, store: Optio
     }
 
     let body = response.text().unwrap();
+    // If not silent mode, show body response
+    if !silent {
+        if let Ok(res_json) = from_str::<Value>(&body) {
+            // Pretty the response when output
+            println!("{}", to_string_pretty(&res_json).unwrap());
+        } else {
+            println!("{}", body);
+        }
+    }
+
+    // Store the response to file
+    if store.is_some() {
+        match File::create(store.unwrap()) {
+            Ok(mut file) => {
+                if let Err(err) = file.write_all(body.as_bytes()) {
+                    eprintln!("Error writing to file: {}", err);
+                }
+            }
+            Err(err) => eprintln!("Error creating file: {}", err),
+        }
+    }
+}
+
+// Handle and print response (blocking)
+async fn handle_response_async(
+    response: reqwest::Response,
+    verbose: bool,
+    silent: bool,
+    store: Option<&String>,
+) {
+    if verbose {
+        println!("Status Code: {}", response.status());
+        println!("Response Headers:\n{:#?}", response.headers());
+    }
+
+    let body = response.text().await.unwrap();
     // If not silent mode, show body response
     if !silent {
         if let Ok(res_json) = from_str::<Value>(&body) {
@@ -363,4 +465,26 @@ fn download_file(mut response: Response, path: &str) -> io::Result<()> {
     println!("\nDownload completed successfully");
 
     Ok(())
+}
+
+// Function to handle sending requests concurrently
+async fn send_request_concurrently(
+    client: reqwest::Client,
+    url: String,
+    method: String,
+    data: Option<String>,
+) -> Result<reqwest::Response, reqwest::Error> {
+    let req = match method.as_str() {
+        "GET" => client.get(&url),
+        "POST" => {
+            let mut post_req = client.post(&url);
+            if let Some(data) = data {
+                post_req = post_req.body(data);
+            }
+            post_req
+        }
+        _ => panic!("Unsupported method!"),
+    };
+
+    req.send().await
 }
